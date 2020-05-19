f9be2a6584d66fb004361d83e625bd24d88606c8
panicmilos99 <panic.milos99@gmail.com>
2020-05-19 15:30:04 +0200

    Added filtering for MultiplierTest.

--- MnozenjeMatrica/MnozenjeMatrica/test/mainTest.cpp
+++ MnozenjeMatrica/MnozenjeMatrica/test/mainTest.cpp
@@ -1,15 +1,39 @@
 #include <gtest\gtest.h>
 
+#include <iostream>
+#include <string>
+
 #ifdef TESTING
 
 extern void readAllMultiplicationElementsFromFiles();
 
-int main(int argc, char** argv) {
-	printf("Running main() from %s\n", __FILE__);
+bool shouldReadMultiplicationElements(int argc, char** argv)
+{
+	if (const bool haveMoreThenOneArgc = argc > 1; haveMoreThenOneArgc)
+	{
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string argument(argv[i]);
 
-	printf("Loading all matrices...\n");
-	readAllMultiplicationElementsFromFiles();
-	printf("Matrices are loaded.\n");
+			if (argument.find("-*MultiplierTest*") != std::string::npos)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+int main(int argc, char** argv) {
+	std::cout << "Running main() from %s" << __FILE__ << '\n';
+
+	if (shouldReadMultiplicationElements(argc, argv)) // --gtest_filter=-*MultiplierTest*
+	{
+		std::cout << "Loading all matrices...\n";
+		readAllMultiplicationElementsFromFiles();
+		std::cout << "Matrices are loaded.\n";
+	}
 
 	testing::InitGoogleTest(&argc, argv);
 
